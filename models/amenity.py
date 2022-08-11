0cc1a6e6b3e6fa0657628bb408ae43efdb403050
ChrissLind <chrislindino@gmail.com>
2022-08-11 18:49:55 -0400

    comments

--- models/amenity.py
+++ models/amenity.py
@@ -8,13 +8,7 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
+    """Represents an Amenity for a MySQL database."""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
